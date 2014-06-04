f26bdfff45cb4957f1cf63b04a3d6dc79a3e6565
Julio Makdisse Saito <saitodisse@gmail.com>
2014-06-04 20:06:17 -0300

    get latest customized version of meld

--- example/todo_mvc_backbone_require/js/main.js
+++ example/todo_mvc_backbone_require/js/main.js
@@ -19,6 +19,13 @@ require.config({
 		backboneLocalstorage: {
 			deps: ['backbone'],
 			exports: 'Store'
+		},
+		Mogger:{
+			deps: [
+				'colorful-logger',
+				'lodash',
+				'meld'
+			]
 		}
 	},
 	paths: {
@@ -28,7 +35,12 @@ require.config({
 		backboneLocalstorage: '../bower_components/backbone.localStorage/backbone.localStorage',
 		text: '../bower_components/requirejs-text/text',
 
-		Mogger: '../bower_components/mogger/src/mogger'
+		Mogger: '../bower_components/mogger/src/mogger',
+		meld: '../bower_components/meld/meld',
+		traceMeld: '../bower_components/meld/aspect/trace',
+		// traceMeld: '../bower_components/meld/aspect/trace',
+		lodash: '../bower_components/lodash/dist/lodash',
+		'colorful-logger': '../bower_components/colorful-logger/src/colorful-logger'
 	}
 });
 
