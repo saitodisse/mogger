a11afcdbab21ba37e17b8f122d64006045da59c9
Julio Makdisse Saito <saitodisse@gmail.com>
2014-07-12 10:43:38 -0300

    new front page, todomvc is an example

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,57 +4,33 @@ module.exports = function(grunt) {
       main: {
         files: [
           {
-            src: ['bower_components/requirejs/require.js'],
-            dest: 'js/vendor/require.js'
+            src: ['bower_components/bootstrap/dist/js/bootstrap.min.js'],
+            dest: 'js/vendor/bootstrap.min.js'
           },
           {
-            src: ['bower_components/jquery/jquery.js'],
-            dest: 'js/vendor/jquery.js'
+            src: ['bower_components/jquery/dist/jquery.min.js'],
+            dest: 'js/vendor/jquery.min.js'
           },
           {
-            src: ['bower_components/underscore/underscore.js'],
-            dest: 'js/vendor/underscore.js'
+            src: ['bower_components/jquery/dist/jquery.min.map'],
+            dest: 'js/vendor/jquery.min.map'
           },
           {
-            src: ['bower_components/backbone/backbone.js'],
-            dest: 'js/vendor/backbone.js'
+            src: ['bower_components/bootstrap/dist/css/bootstrap-theme.min.css'],
+            dest: 'css/bootstrap-theme.min.css'
           },
           {
-            src: ['bower_components/backbone.localStorage/backbone.localStorage.js'],
-            dest: 'js/vendor/backbone.localStorage.js'
+            src: ['bower_components/bootstrap/dist/css/bootstrap.min.css'],
+            dest: 'css/bootstrap.min.css'
           },
           {
-            src: ['bower_components/requirejs-text/text.js'],
-            dest: 'js/vendor/text.js'
+            expand: true,
+            cwd: 'bower_components/bootstrap/dist/fonts/',
+            src: '**',
+            dest: 'fonts/',
+            flatten: true,
+            filter: 'isFile'
           },
-          {
-            src: ['bower_components/mogger/src/mogger.js'],
-            dest: 'js/vendor/mogger.js'
-          },
-          {
-            src: ['bower_components/meld/meld.js'],
-            dest: 'js/vendor/meld.js'
-          },
-          {
-            src: ['bower_components/meld/aspect/trace.js'],
-            dest: 'js/vendor/trace.js'
-          },
-          {
-            src: ['bower_components/lodash/dist/lodash.js'],
-            dest: 'js/vendor/lodash.js'
-          },
-          {
-            src: ['bower_components/colorful-logger/src/colorful-logger.js'],
-            dest: 'js/vendor/colorful-logger.js'
-          },
-          {
-            src: ['bower_components/todomvc-common/base.css'],
-            dest: 'css/base.css'
-          },
-          {
-            src: ['bower_components/todomvc-common/bg.png'],
-            dest: 'css/bg.png'
-          }
         ]
       }
     }
