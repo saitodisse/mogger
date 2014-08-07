897ccd69e3a7dd672bcd84794e1855ff2faf4141
Julio Makdisse Saito <saitodisse@gmail.com>
2014-08-07 13:04:22 -0300

    publishPatch.sh updated

--- publishPatch.sh
+++ publishPatch.sh
@@ -3,7 +3,16 @@
 while true; do
     read -p "Do you want to publish a patch? [y/n]: " yn
     case $yn in
-        [Yy]* ) bower version patch; sudo npm version patch; git add .; git commit -m'publishing a new patch';git push; sudo npm publish .; break;;
+        [Yy]* ) npm version patch; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+while true; do
+    read -p "please, update version on bower.json. Did you do it? [y/n]: " yn
+    case $yn in
+        [Yy]* ) git add .; git commit -m'bower updating version'; git push; sudo npm publish .; break;;
         [Nn]* ) exit;;
         * ) echo "Please answer yes or no.";;
     esac
