31b8cd299cc6cab083cb0188dcb4cb7101db41fb
Julio Makdisse Saito <saitodisse@gmail.com>
2014-09-04 17:49:41 -0300

    cover in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,47 @@
 var gulp = require('gulp');
-var watch = require('gulp-watch');
-var gutil = require('gulp-util');
-var mocha = require('gulp-mocha');
-// var grep = require('gulp-grep-stream');
-// var plumber = require('gulp-plumber');
 
-gulp.task('default', ['mocha', 'watch-mocha']);
+var sources = ['*.js', 'src/**/*.js', '!gulpfile.js'];
+var testSources = ['test/*.test.js'];
+var allSources = sources.concat(testSources);
 
+gulp.task('default', ['test', 'watch-mocha']);
+
+gulp.task('test', ['cover', 'mocha']);
 
 gulp.task('mocha', function() {
-    return gulp.src(['test/*.test.js'], { read: false })
+    var mocha = require('gulp-mocha');
+    var gutil = require('gulp-util');
+
+    return gulp.src(testSources, { read: false })
         .pipe( mocha( {
-        	reporter: 'spec', growl: 'true'
+            reporter: 'spec', growl: 'true'
         } ))
         .on('error', gutil.log);
 });
 
 gulp.task('watch-mocha', function() {
-    gulp.watch(['lib/**', 'test/*.test.js'], ['mocha']);
+    gulp.watch(allSources, ['cover', 'mocha']);
+});
+
+// Run tests and output reports
+gulp.task('cover', function (cb) {
+    var mocha = require('gulp-mocha');
+    var istanbul = require('gulp-istanbul');
+
+    gulp.src(sources)
+        .pipe(istanbul()) // Covering files
+        .on('finish', function () {
+            gulp.src(testSources)
+                .pipe(mocha())
+                .pipe(istanbul.writeReports()) // Creating the reports after tests runned
+                .on('end', cb);
+    });
+});
+
+gulp.task('start_cover', function (cb) {
+  var istanbul = require('gulp-istanbul');
+
+  gulp.src(sources)
+    .pipe(istanbul())
+    .on('end', cb);
 });
