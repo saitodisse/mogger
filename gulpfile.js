6aac719dde8eb396c84aa70f469089912a77b99a
Julio Makdisse Saito <saitodisse@gmail.com>
2014-09-05 01:06:24 -0300

    disabling console.log does not work because the output is async. I will revert on next commit

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var allSources = sources.concat(testSources);
 
 gulp.task('default', ['cover', 'watch']);
 
+gulp.task('console', ['disable_console_log', 'cover', 'enable_console_log', 'mocha']);
 
 gulp.task('watch', function() {
     gulp.watch(allSources, ['cover']);
@@ -14,35 +15,63 @@ gulp.task('watch', function() {
 
 
 var originalConsoleLog = console.log;
-gulp.task('disable-console-log', function() {
+
+gulp.task('disable_console_log', function() {
     originalConsoleLog = console.log;
     console.log = function () {};
 });
 
-gulp.task('enable-console-log', function() {
+gulp.task('enable_console_log', function() {
     console.log = originalConsoleLog;
 });
 
+gulp.task('mocha', function() {
+    var mocha = require('gulp-mocha');
+    var gutil = require('gulp-util');
+
+    return gulp.src(testSources, { read: false })
+        .pipe( mocha( {
+            reporter: 'spec', growl: 'true'
+        } ))
+        .on('error', gutil.log);
+});
 
 
-gulp.task('cover', ['disable-console-log', 'enable-console-log'], function () {
+gulp.task('mocha', function() {
+    var mocha = require('gulp-mocha');
+    var gutil = require('gulp-util');
+
+    return gulp.src(testSources, { read: false })
+        .pipe( mocha( {
+            reporter: 'spec', growl: 'true'
+        } ))
+        .on('error', gutil.log);
+});
+
+gulp.task('cover', function () {
     var mocha = require('gulp-mocha');
     var gutil = require('gulp-util');
     var istanbul = require('gulp-istanbul');
+    var es = require('event-stream');
 
-
-    // Instambul
     return gulp.src(sources)
-        .pipe( istanbul() )
+        .pipe(istanbul())
         .on('finish', function () {
             //console.log = original;
             gulp.src(testSources)
                 .pipe( mocha({ reporter: 'spec', growl: 'true' }) )
                 .on('error', gutil.log) // prevent error to stop watch
 
-                //.pipe(logCapture.start(fs, 'writeFileSync'))
-                .pipe(istanbul.writeReports()) // Creating the reports after tests runned
-                //.pipe(logCapture.stop('xml'))
-    ;
-    });
+
+                .pipe(istanbul.writeReports())
+
+                // turn this async function into a stream
+                //.on('end', ) // prevent error to stop watch
+                // .pipe(es.mapSync(function (data, cb) {
+                //     //continue
+                //     finished();
+                //     cb(null, data);
+                // }))
+           ;
+        });
 });
