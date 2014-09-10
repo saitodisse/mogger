37685a2804fd2eea815639ebd9e5889864ddcc1e
Julio Makdisse Saito <saitodisse@gmail.com>
2014-09-10 17:10:58 -0300

    example 06

--- _main_src/src/javascript/data/all-examples.js
+++ _main_src/src/javascript/data/all-examples.js
@@ -28,5 +28,23 @@ module.exports = function() {
 			subTitle: 'log can be colored',
 			script: require('../pages/examples/ex-03-src')
 		},
+		{
+			id: 'ex-04',
+			title: 'others css',
+			subTitle: 'change other css properties',
+			script: require('../pages/examples/ex-04-src')
+		},
+		{
+			id: 'ex-05',
+			title: 'paddind size',
+			subTitle: 'everything can be correctly indented',
+			script: require('../pages/examples/ex-05-src')
+		},
+		{
+			id: 'ex-06',
+			title: 'title vs target css',
+			subTitle: 'the title and the target do not require the same CSS',
+			script: require('../pages/examples/ex-06-src')
+		},
 	];
 };
---
+++ _main_src/src/javascript/pages/examples/ex-06-src.js
@@ -0,0 +1,75 @@
+'use strict';
+
+var Mogger = require('mogger');
+
+/*
+
+file:     ex-05-src.js
+class:    Example05Src
+instance: example05Src
+
+*/
+
+module.exports = function() {
+
+	/**
+	 * an object with two methods
+	 */
+	var simple_obj_1 = {
+	  	method_1_A: function() {},
+	  	method_1_B: function() {}
+	};
+	var simple_obj_2 = {
+	  	method_2_A: function() {},
+	  	method_2_B: function() {}
+	};
+
+	/**
+	 * instantiate mogger
+	 * add the surrogateTargets array.
+	 */
+	var mogger = new Mogger({
+		surrogateTargets: [
+
+		{
+			title: 'SIMPLE_OBJ_1',
+			target: simple_obj_1
+		},
+
+		{
+			title: 'SIMPLE_OBJ_2',
+			target: simple_obj_2
+		}
+
+		]
+	});
+
+	/**
+	 * tracing all methods from simple_obj_1
+	 */
+	mogger.traceObj({
+    	before: { message: 'Simple Obj 1', css: 'color: #266;', size: 16 },
+    	targetTitle: 'SIMPLE_OBJ_1',
+    	localTargetConfig: { css: 'color: #299;' }
+  	});
+	mogger.traceObj({
+    	before: { message: 'Obj 2', css: 'color: #626;', size: 16 },
+    	targetTitle: 'SIMPLE_OBJ_2',
+    	localTargetConfig: { css: 'color: #929;' }
+  	});
+
+
+	/**
+	 * call the simple_obj_1 methods
+	 * tip: see console.logs message
+	 */
+	var callSources = function () {
+		simple_obj_1.method_1_A();
+		simple_obj_1.method_1_B();
+		simple_obj_2.method_2_A();
+		simple_obj_2.method_2_B();
+	};
+
+	callSources();
+
+};
