e292c52ea37a2619f96015176b34400d5577e3ea
Julio Makdisse Saito <saitodisse@gmail.com>
2014-09-10 16:56:29 -0300

    example 3

--- _main_src/src/javascript/data/all-examples.js
+++ _main_src/src/javascript/data/all-examples.js
@@ -8,8 +8,6 @@ instance: allExamples
 
 */
 
-
-
 module.exports = function() {
 	return [
 		{
@@ -23,6 +21,12 @@ module.exports = function() {
 			title: 'Titles',
 			subTitle: 'you can define titles',
 			script: require('../pages/examples/ex-02-src')
+		},
+		{
+			id: 'ex-03',
+			title: 'colored logs',
+			subTitle: 'log can be colored',
+			script: require('../pages/examples/ex-03-src')
 		},
 	];
 };
