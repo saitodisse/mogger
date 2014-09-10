33f93cbbbccda51a3cd6303c926f37ee069d5b21
Julio Makdisse Saito <saitodisse@gmail.com>
2014-09-10 17:12:56 -0300

    convert tabs to spaces on examples

--- _main_src/src/javascript/pages/examples/ex-01-src.js
+++ _main_src/src/javascript/pages/examples/ex-01-src.js
@@ -12,42 +12,42 @@ instance: example01Src
 
 module.exports = function() {
 
-	/**
-	 * an object with two methods
-	 */
-	var simple_obj = {
-	  	methodA: function() {},
-	  	methodB: function() {}
-	};
+    /**
+     * an object with two methods
+     */
+    var simple_obj = {
+        methodA: function() {},
+        methodB: function() {}
+    };
 
-	/**
-	 * instantiate mogger
-	 * add the surrogateTargets array.
-	 */
-	var mogger = new Mogger({
-		surrogateTargets: [{
-			title: 'SIMPLE_OBJ',
-			target: simple_obj
-		}]
-	});
+    /**
+     * instantiate mogger
+     * add the surrogateTargets array.
+     */
+    var mogger = new Mogger({
+        surrogateTargets: [{
+            title: 'SIMPLE_OBJ',
+            target: simple_obj
+        }]
+    });
 
-	/**
-	 * tracing all methods from simple_obj
-	 */
-	mogger.traceObj({
-    	targetTitle: 'SIMPLE_OBJ'
-  	});
+    /**
+     * tracing all methods from simple_obj
+     */
+    mogger.traceObj({
+        targetTitle: 'SIMPLE_OBJ'
+    });
 
 
-	/**
-	 * call the simple_obj methods
-	 * tip: see console.logs message
-	 */
-	var callSources = function () {
-		simple_obj.methodA();
-		simple_obj.methodB();
-	};
+    /**
+     * call the simple_obj methods
+     * tip: see console.logs message
+     */
+    var callSources = function () {
+        simple_obj.methodA();
+        simple_obj.methodB();
+    };
 
-	callSources();
+    callSources();
 
 };
--- _main_src/src/javascript/pages/examples/ex-02-src.js
+++ _main_src/src/javascript/pages/examples/ex-02-src.js
@@ -12,43 +12,43 @@ instance: example02Src
 
 module.exports = function() {
 
-	/**
-	 * an object with two methods
-	 */
-	var simple_obj = {
-	  	methodA: function() {},
-	  	methodB: function() {}
-	};
+    /**
+     * an object with two methods
+     */
+    var simple_obj = {
+        methodA: function() {},
+        methodB: function() {}
+    };
 
-	/**
-	 * instantiate mogger
-	 * add the surrogateTargets array.
-	 */
-	var mogger = new Mogger({
-		surrogateTargets: [{
-			title: 'SIMPLE_OBJ',
-			target: simple_obj
-		}]
-	});
+    /**
+     * instantiate mogger
+     * add the surrogateTargets array.
+     */
+    var mogger = new Mogger({
+        surrogateTargets: [{
+            title: 'SIMPLE_OBJ',
+            target: simple_obj
+        }]
+    });
 
-	/**
-	 * tracing all methods from simple_obj
-	 */
-	mogger.traceObj({
-    	before: { message: 'Simple Obj: ' },
-    	targetTitle: 'SIMPLE_OBJ'
-  	});
+    /**
+     * tracing all methods from simple_obj
+     */
+    mogger.traceObj({
+        before: { message: 'Simple Obj: ' },
+        targetTitle: 'SIMPLE_OBJ'
+    });
 
 
-	/**
-	 * call the simple_obj methods
-	 * tip: see console.logs message
-	 */
-	var callSources = function () {
-		simple_obj.methodA();
-		simple_obj.methodB();
-	};
+    /**
+     * call the simple_obj methods
+     * tip: see console.logs message
+     */
+    var callSources = function () {
+        simple_obj.methodA();
+        simple_obj.methodB();
+    };
 
-	callSources();
+    callSources();
 
 };
