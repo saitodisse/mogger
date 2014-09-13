a8fd602a82fd4561dd11370b11513b36695f48c4
Julio Makdisse Saito <saitodisse@gmail.com>
2014-09-12 21:05:35 -0300

    trying woopra

--- _main_src/src/javascript/app.js
+++ _main_src/src/javascript/app.js
@@ -101,14 +101,14 @@ module.exports = {
         // window.ga('create', 'UA-54745828-1', 'auto');
 
         //woopra
-        (function(){
-            var t,i,e,n=window,o=document,a=arguments,s="script",r=["config","track","identify","visit","push","call","trackForm","trackClick"],c=function(){var t,i=this;for(i._e=[],t=0;r.length>t;t++)(function(t){i[t]=function(){return i._e.push([t].concat(Array.prototype.slice.call(arguments,0))),i}})(r[t])};for(n._w=n._w||{},t=0;a.length>t;t++)n._w[a[t]]=n[a[t]]=n[a[t]]||new c;i=o.createElement(s),i.async=1,i.src="//static.woopra.com/js/w.js",e=o.getElementsByTagName(s)[0],e.parentNode.insertBefore(i,e)
-        })("woopra");
+        // (function(){
+        //     var t,i,e,n=window,o=document,a=arguments,s="script",r=["config","track","identify","visit","push","call","trackForm","trackClick"],c=function(){var t,i=this;for(i._e=[],t=0;r.length>t;t++)(function(t){i[t]=function(){return i._e.push([t].concat(Array.prototype.slice.call(arguments,0))),i}})(r[t])};for(n._w=n._w||{},t=0;a.length>t;t++)n._w[a[t]]=n[a[t]]=n[a[t]]||new c;i=o.createElement(s),i.async=1,i.src="//static.woopra.com/js/w.js",e=o.getElementsByTagName(s)[0],e.parentNode.insertBefore(i,e)
+        // })("woopra");
 
-        woopra.config({
-            domain: 'saitodisse.github.io'
-        });
-        woopra.track();
+        // woopra.config({
+        //     domain: 'saitodisse.github.io'
+        // });
+        // woopra.track();
     },
 
     // This is how you navigate around the app.
