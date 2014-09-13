66e56c1ab0df5569f1cf81298eb3f8dc2e147344
Julio Makdisse Saito <saitodisse@gmail.com>
2014-09-13 12:56:07 -0300

    style generator is working very well on google chrome and firebug

--- _main_src/src/javascript/pages/style-generator-page.js
+++ _main_src/src/javascript/pages/style-generator-page.js
@@ -17,35 +17,34 @@ module.exports = BasePage.extend({
     template: require('../templates/style-generator')(configuration),
 
     events:{
-    	'input  [data-hook=font-size-range]'         : 'setBeforeSize',
-    	'change [data-hook=font-size-text]'          : 'setBeforeSize',
+    	'input  [data-hook = font-size-range]'         : 'setBeforeSize',
+    	'change [data-hook = font-size-text]'          : 'setBeforeSize',
 
-    	'change [data-hook=before-color-picker]'     : 'setBeforeColor',
-    	'change [data-hook=before-color-picker-text]': 'setBeforeColor',
+    	'change [data-hook = before-color-picker]'     : 'setBeforeColor',
+    	'change [data-hook = before-color-picker-text]': 'setBeforeColor',
 
-    	'change [data-hook=target-color-picker]'     : 'setTargetColor',
-    	'change [data-hook=target-color-picker-text]': 'setTargetColor',
+    	'change [data-hook = target-color-picker]'     : 'setTargetColor',
+    	'change [data-hook = target-color-picker-text]': 'setTargetColor',
     },
 
     render: function () {
         this.renderWithTemplate(this);
 
-        this.styleGeneratorExecute = new StyleGeneratorExecute({
-        	model: this.model
+		// listen to model
+        this.listenTo(this.model, 'change:beforeSize', this.beforeSizeChanged);
+        this.listenTo(this.model, 'change:beforeColor', this.beforeColorChanged);
+        this.listenTo(this.model, 'change:targetColor', this.targetColorChanged);
+
+        this.listenTo(this.model, 'change', function(model) {
+    		this.cleanAndShowLogs(model);
         });
-		this.styleGeneratorExecute.setModel(this.model);
-        this.styleGeneratorExecute.createMogger();
-        this.cleanAndShowLogs();
+
+        this.initializeLogs();
 
 		// initialize from model
 		this.beforeSizeChanged(this.model);
 		this.beforeColorChanged(this.model);
 		this.targetColorChanged(this.model);
-
-		// listen to model
-        this.listenTo(this.model, 'change:beforeSize', this.beforeSizeChanged);
-        this.listenTo(this.model, 'change:beforeColor', this.beforeColorChanged);
-        this.listenTo(this.model, 'change:targetColor', this.targetColorChanged);
 
         return this;
     },
@@ -67,24 +66,33 @@ module.exports = BasePage.extend({
     beforeSizeChanged: function(model) {
     	this.queryByHook('font-size-range').value = model.beforeSize;
     	this.queryByHook('font-size-text').value = model.beforeSize;
-    	this.cleanAndShowLogs();
     },
     beforeColorChanged: function(model) {
     	this.queryByHook('before-color-picker').value = model.beforeColor;
     	this.queryByHook('before-color-picker-text').value = model.beforeColor;
-    	this.cleanAndShowLogs();
     },
     targetColorChanged: function(model) {
     	this.queryByHook('target-color-picker').value = model.targetColor;
     	this.queryByHook('target-color-picker-text').value = model.targetColor;
-    	this.cleanAndShowLogs();
     },
 
-    // clean and show logs
-    cleanAndShowLogs: function() {
+
+
+    initializeLogs: function() {
+        this.styleGeneratorExecute = new StyleGeneratorExecute({
+        	model: this.model
+        });
 		this.styleGeneratorExecute.setModel(this.model);
+        this.styleGeneratorExecute.createMogger();
+        this.cleanAndShowLogs(this.model);
+    },
+
+    cleanAndShowLogs: function(model) {
+		this.styleGeneratorExecute.setModel(model);
 		this.styleGeneratorExecute.clearConsole();
 		this.styleGeneratorExecute.createMogger();
 		this.styleGeneratorExecute.callSources();
-    }
+    },
+
+
 });
