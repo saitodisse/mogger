f2637f8e09649324f1d8be4a5c3c5cac387236d0
Julio Makdisse Saito <saitodisse@gmail.com>
2014-12-26 13:57:13 -0200

    Updating docs

--- _main_src/gulp/tasks/fonts.js
+++ _main_src/gulp/tasks/fonts.js
@@ -5,7 +5,7 @@ var gulp = require('gulp');
 gulp.task('fonts', function() {
 
   var source = './node_modules/bootstrap-sass/assets/fonts/bootstrap/**';
-  var dest = './build/bootstrap';
+  var dest = './build/fonts/bootstrap';
 
   return gulp.src(source)
     .pipe(gulp.dest(dest));
